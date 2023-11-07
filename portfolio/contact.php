4b11af4cf595d321bb2ee141917d80d8c02f7f5d
theZalmanian <thezalmanian@gmail.com>
2023-11-07 02:59:02 -0800

    Updated portfolio pages to utilize latest changes

--- portfolio/contact.php
+++ portfolio/contact.php
@@ -1,28 +1,23 @@
+<?php 
+    // get access to all helper methods
+    require_once("../layouts/helpers.php"); 
+
+    // save the current pages name to session
+    setCurrentPage("Contact Me");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact</title>
-    <link rel="icon" type="image/x-icon" href="/images/theZalmanian.jpg">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/site.css">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script type="module" src="/js/helpers.js"></script>
-    <script type="module" src="/js/theme-switcher.js"></script>
+    <?php 
+        // include standard header metadata
+        require "../layouts/standard-page-metadata.php"; 
+    ?>
 </head>
 <body>
     <?php 
-        // open access to the session
-        session_start();
-
-        // save the current page
-        $_SESSION["currentPage"] = "Contact Me";
-
         // display site navigation
-        require "../layouts/navigation.php";   
+        require "../layouts/navigation.php";     
     ?>
     <main class="container">
         <div class="row">
