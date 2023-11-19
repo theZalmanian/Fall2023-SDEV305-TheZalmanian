b87c795f59c0de5d097f5fc9d989a08ed5ee315f
theZalmanian <thezalmanian@gmail.com>
2023-11-19 14:44:13 -0800

    (#13) Removed session related code and checks from helpers.php

--- php/helpers.php
+++ php/helpers.php
@@ -1,28 +1,4 @@
 <?php 
-    // only start a session if one does not exist yet
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    /**
-     * Gets and returns the page that is currently open from Session Storage, if any
-     * @return string the page that is currently open from Session Storage, if present; otherwise empty string
-     */
-    function getCurrentPage() {
-        // return the page that is currently open from Session Storage, 
-        // if one is not present, return an empty string
-        return isset($_SESSION["currentPage"]) ? $_SESSION["currentPage"] : ""; 
-    }
-
-    /**
-     * Sets the current page in Session Storage to be the given page
-     * @param string $pageTitle The page being saved as the current page in Session Storage
-     */
-    function setCurrentPage($pageTitle) {
-        // save the given pages name to session as the current page
-        $_SESSION["currentPage"] = $pageTitle;
-    }
-
     /**
      * Returns a Bootstrap Alert to be displayed containing the given title and message, 
      * along with the given link styled as a button at the bottom
