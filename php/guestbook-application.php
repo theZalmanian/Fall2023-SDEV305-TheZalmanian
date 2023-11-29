08529168ada49f71a76ca2e8ea86bc2866f56f97
theZalmanian <thezalmanian@gmail.com>
2023-11-29 07:20:07 -0800

    (#16) Created a guestbook-application page - Moved the form on guestbook.php to it

---
+++ php/guestbook-application.php
@@ -0,0 +1,58 @@
+<?php 
+    // get access to all PHP helpers
+    require_once("/home/thezalma/public_html/initial.php");
+
+    // save the current pages name to session
+    $currPageTitle = "Guestbook Application";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        // include standard header metadata
+        require_once(LAYOUTS_PATH . "/standard-page-metadata.php");
+    ?>
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-md-2 col-lg-3">
+            </div>
+            <div class="col-md-8 col-lg-6">
+                <div class="card p-2 text-center">
+                    <h1>
+                        Guestbook Application
+                    </h1> 
+                </div>
+
+                <form class="my-2" id="contact-form" action="../php/send-email.php" method="post">
+                    <div class="card p-3 my-1">
+                        <div class="contact form-floating">
+                            <input type="text" class="form-control" id="name" name="name"
+                                placeholder="" required>
+                            <label for="name">
+                                Name <span class="text-danger">*</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="card p-3 my-1">
+                        <div class="contact form-floating">
+                            <textarea class="form-control" id="message" name="message"
+                                placeholder="" required></textarea>
+                            <label for="message">
+                                Message <span class="text-danger">*</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="card p-3 my-1">
+                        <button class="btn py-2 border" id="submit-contact">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-2 col-lg-3">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
--- portfolio/guestbook.php
+++ portfolio/guestbook.php
@@ -24,44 +24,7 @@
             <div class="col-md-2 col-lg-3">
             </div>
             <div class="col-md-8 col-lg-6">
-                <div class="card p-2 text-center">
-                    <h1>
-                        Guestbook
-                    </h1> 
-                </div>
-
-                <form class="my-2" id="contact-form" action="../php/send-email.php" method="post">
-                    <div class="card p-3 my-1">
-                        <div class="contact form-floating">
-                            <input type="text" class="form-control" id="name" name="name"
-                                placeholder="" required>
-                            <label for="name">
-                                Name <span class="text-danger">*</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="card p-3 my-1">
-                        <div class="contact form-floating">
-                            <input type="email" class="form-control" id="email" name="email" 
-                                placeholder="" required>
-                            <label for="email">
-                                Email Address <span class="text-danger">*</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="card p-3 my-1">
-                        <div class="contact form-floating">
-                            <textarea class="form-control" id="message" name="message"
-                                placeholder="" required></textarea>
-                            <label for="message">
-                                Message <span class="text-danger">*</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="card p-3 my-1">
-                        <button class="btn py-2 border" id="submit-contact">Submit</button>
-                    </div>
-                </form>
+                
             </div>
             <div class="col-md-2 col-lg-3">
             </div>
