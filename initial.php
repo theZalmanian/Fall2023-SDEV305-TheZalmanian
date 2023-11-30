041dfeaed0d52d708bc23ecf0925b92494b77e95
theZalmanian <thezalmanian@gmail.com>
2023-11-29 19:35:14 -0800

    (#17) Updated send-email.php to store user's given name in cookie

--- initial.php
+++ initial.php
@@ -8,6 +8,9 @@
     define("PHP_PATH", PUBLIC_HTML_PATH . "/php");
     define("LAYOUTS_PATH", PHP_PATH . "/layouts");
 
+    // define cookie keys
+    define("NAME_KEY", "user-name");
+
     // provide DB credentials and connect/disconnect functions
     require_once('/home/thezalma/db-connect-guestbook.php');
     
--- php/send-email.php
+++ php/send-email.php
@@ -4,6 +4,28 @@
 
     // save the current pages name to session
     $currPageTitle = "Send Application";
+
+    /**
+     * The name submitted on the Guestbook Application page
+     */
+    $name = $_POST["name"];
+
+    /**
+     * The message submitted, awaiting approval to be displayed in the Guestbook
+     */
+    $message = $_POST["message"];
+
+    // setup trackers 
+    $applicationSubmitted = isset($name) && isset($message);
+    $messageSent = false;
+
+    if($applicationSubmitted) {
+        // save the given name in a cookie, have it expire after 1y, and global across site
+        setcookie(NAME_KEY, $name, time() + (60 * 60 * 24 * 365), "/");
+
+        // attempt to send email with given data
+        $messageSent = setupAndSendEmail($name, $message);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,51 +39,31 @@
     <body>
         <main class="mt-3">
             <?php
-                // setup variables and check that all required inputs were 
-                // submitted on the Guestbook Application form
-                $name = $_POST["name"];
-                $message = $_POST["message"];
-
-                if( isset($name) && isset($message) ) {
-                    // setup sending and receiving addresses
-                    $sendToAddress = "";
-                    $sendFromAddress = "GuestbookApplication@thezalmanian.greenriverdev.com";
-
-                    // setup headers for html email
-                    $headers = "MIME-Version: 1.0" . "\r\n";
-                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-                    $headers .= "From: $sendFromAddress" . "\r\n";
-
-                    $subject = "Guestbook Application";
-
-                    // attempt to send email with given data
-                    $messageSent = mail($sendToAddress, $subject, setupEmailContent($name, $message), $headers);
-
-                    // if the message was sent
-                    if($messageSent) {
-                        // display success to user
-                        echo generateMessageWithLink("/portfolio/guestbook.php", "Guestbook", 
-                                                     "Your application was submitted successfully!");
-
-                        // insert given guestbook entry to DB, as unpublished
-                        executeQuery("INSERT INTO GuestbookEntries (Name, Message, Published) 
-                                        VALUES ('{$name}', '{$message}', '0')");
-
-                    }
-
-                    // if the message was not sent, display error 
-                    else {
-                        echo generateMessageWithLink("/portfolio/guestbook.php", "Guestbook",
-                                                     "Please try again later", 
-                                                     "ERROR: Your application could not be submitted at this time");
-                    }
-                }  
-                
-                // otherwise display error and link to Guestbook Application form
-                else {                
+                // if the application was not submitted yet
+                if(!$applicationSubmitted) {
+                    // display error, and link to Guestbook application page
                     echo generateMessageWithLink("/php/guestbook-application.php", "Guestbook Application", 
                                                  "Please fill out the form and try again",
                                                  "ERROR: No submission received from Guestbook Application");
+                }
+
+                // if the application was submitted & the message was sent successfully
+                elseif($applicationSubmitted && $messageSent) {
+                    // display success to user, and link to Guestbook page
+                    echo generateMessageWithLink("/portfolio/guestbook.php", "Guestbook", 
+                                                 "Your application was submitted successfully!");
+
+                    // insert given guestbook entry to DB, marked as unpublished (0)
+                    executeQuery("INSERT INTO GuestbookEntries (Name, Message, Published) 
+                                    VALUES ('{$name}', '{$message}', '0')");
+                }
+
+                // if the application was submitted & message could not be sent
+                elseif($applicationSubmitted && !$messageSent) {
+                    // display error, and link to Guestbook page
+                    echo generateMessageWithLink("/portfolio/guestbook.php", "Guestbook",
+                                                 "Please try again later", 
+                                                 "ERROR: Your application could not be submitted at this time");
                 }
             ?>
         </main>
@@ -70,34 +72,56 @@
 
 <?php 
     /**
-     * Generates and returns HTML content for the email using the given data
-     * @param string $name the name of the person sending the message
-     * @param string $email the email address of the person sending the message
-     * @param string $message the message being sent by email
+     * Generates an HTML email using the given content and attempts to send it to the admin address.
+     * Returns whether the email was sent successfully (true/false)
+     * @param string $name the name of the person leaving a Guestbook entry
+     * @param string $message the message they left behind, awaiting approval
+     * @return bool true if email was sent successfully; otherwise false
+     */
+    function setupAndSendEmail($name, $message) {
+        // setup sending and receiving addresses
+        $sendToAddress = "";
+        $sendFromAddress = "GuestbookApplication@thezalmanian.greenriverdev.com";
+
+        // setup headers for html email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: $sendFromAddress" . "\r\n";
+        $subject = "Guestbook Application";
+
+        // attempt to send email with given data
+        $messageSent = mail($sendToAddress, $subject, setupHtmlEmailContent($subject, $name, $message), $headers);
+
+        // return the result of sending message (success/fail)
+        return $messageSent;
+    }
+
+    /**
+     * Generates and returns HTML content for the approval email using the given data
+     * @param string $subject the subject of the email, will be displayed at top of message content
+     * @param string $name the name of the person leaving a Guestbook entry
+     * @param string $message the message they left behind, awaiting approval
      * @return string HTML content for the email message
      */
-    function setupEmailContent($name, $message) {
-        // setup most of email content
-        $emailContent = "<html lang='en'>
-                            <head>
-                                <title>Guestbook Application</title>
-                            </head>
-                            <body>
-                                <h3>
-                                    <i>Guestbook Application</i>
-                                </h3>
-                                <p>
-                                    {$message}
-                                </p>
-                                <p>
-                                    Sincerely,
-                                    <br>
-                                    {$name}
-                                </p>
-                            </body>
-                        </html>";
-
-        // return the generated email content
-        return $emailContent;
+    function setupHtmlEmailContent($subject, $name, $message) {
+        // setup and return HTML email content
+        return "<html lang='en'>
+                    <head>
+                        <title>{$subject}</title>
+                    </head>
+                    <body>
+                        <h3>
+                            <i>{$subject}</i>
+                        </h3>
+                        <p>
+                            {$message}
+                        </p>
+                        <p>
+                            Sincerely,
+                            <br>
+                            {$name}
+                        </p>
+                    </body>
+                </html>";
     }
 ?>
