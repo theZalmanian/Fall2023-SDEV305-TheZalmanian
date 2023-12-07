7c8dfffb3654f49ffa55c42010924c7bea4dcbbe
theZalmanian <thezalmanian@gmail.com>
2023-12-07 01:33:42 -0800

    (#18) Created approve-entry.php

---
+++ php/approve-entry.php
@@ -0,0 +1,29 @@
+<?php 
+    // get access to all PHP helpers
+    require_once("/home/thezalma/public_html/initial.php");
+
+    // save the current pages name to session
+    $currPageTitle = "Approve Entry";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        // include standard header metadata
+        require_once(LAYOUTS_PATH . "/standard-page-metadata.php");
+    ?>
+</head>
+<body>
+    <main class="container mt-3">
+        <div class="row">
+            <div class="col-md-2 col-lg-3">
+            </div>
+            <div class="col-md-8 col-lg-6">
+                
+            </div>
+            <div class="col-md-2 col-lg-3">
+            </div>
+        </div>
+    </main>
+</body>
